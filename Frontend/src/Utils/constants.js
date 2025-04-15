6ec926f2b24aff30f1d1c299ae0f29f51edf2df3
Kausar Khan <kausarrkhan83@gmail.com>
2025-04-15 17:32:40 +0530

    set backend  domain

--- Frontend/src/Utils/constants.js
+++ Frontend/src/Utils/constants.js
@@ -1,5 +1,7 @@
 export const DEFAULT_PROFILE =
   "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR0_XPfyUZJugz5lXkm0DUtAkpjRw367tcFig&s";
 
-export const BASE_URL = "http://localhost:8080"
-// https://novamart-mygy.onrender.com
+const origin = true;
+export const BASE_URL = origin
+  ? "https://novamart-mygy.onrender.com"
+  : "http://localhost:8080";
