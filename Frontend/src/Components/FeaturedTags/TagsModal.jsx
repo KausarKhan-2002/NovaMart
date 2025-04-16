368562a143cee2b29d1ca0916c60e18fe7700feb
Kausar Khan <kausarrkhan83@gmail.com>
2025-04-17 00:22:32 +0530

    Fix tag modal

--- Frontend/src/Components/FeaturedTags/TagsModal.jsx
+++ Frontend/src/Components/FeaturedTags/TagsModal.jsx
@@ -2,9 +2,9 @@ import React from "react";
 
 function TagsModal({ nextType, cancelSwitch, confirmTagTypeSwitch }) {
   return (
-    <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50">
-      <div className="bg-white dark:bg-gray-800 p-6 rounded-lg shadow-lg max-w-md w-full">
-        <h2 className="text-lg font-semibold text-gray-800 dark:text-white mb-3">
+    <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50 px-6 md:px-0">
+      <div className="bg-white p-6 rounded-lg shadow-lg max-w-md w-full">
+        <h2 className="text-lg font-semibold text-gray-800">
           Change Tag Type?
         </h2>
         <p className="text-sm text-gray-700 dark:text-gray-300 mb-4">
@@ -14,7 +14,7 @@ function TagsModal({ nextType, cancelSwitch, confirmTagTypeSwitch }) {
         <div className="flex justify-end gap-3">
           <button
             onClick={cancelSwitch}
-            className="px-4 py-2 bg-gray-200 dark:bg-gray-700 text-gray-800 dark:text-white rounded-md"
+            className="px-4 py-2 bg-gray-200 text-gray-800 dark:text-white rounded-md"
           >
             Cancel
           </button>
