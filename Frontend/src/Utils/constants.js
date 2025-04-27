abafa43641b09ba611cf5a9409abd9c390148e6c
Kausar Khan <kausarrkhan83@gmail.com>
2025-04-27 18:54:54 +0530

    switch remote origin free domain

--- Frontend/src/Utils/constants.js
+++ Frontend/src/Utils/constants.js
@@ -3,7 +3,7 @@ export const DEFAULT_PROFILE =
   "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR0_XPfyUZJugz5lXkm0DUtAkpjRw367tcFig&s";
 
 // BASE_URL for backend
-const origin = false;
+const origin = true;
 export const BASE_URL = origin
   ? "https://novamart-mygy.onrender.com"
   : "http://localhost:8080";
